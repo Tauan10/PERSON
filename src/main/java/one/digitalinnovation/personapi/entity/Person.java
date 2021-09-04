933b049b155da8ca15c57af8544aca5faa6a3b71
Tauan Mateus[git config --global user.name tauan <tauanmateus1002@gmail.com>
2021-09-03 21:12:27 -0300

    funcionalidade pessoa

--- src/main/java/one/digitalinnovation/personapi/entity/Person.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity
@@ -28,7 +29,7 @@ public class Person {
     @Column(nullable = false, unique = true)
     private String cpf;
 
-    private String birthDate;
+    private LocalDate birthDate;
 
     @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
     private List<Phone> phones;
